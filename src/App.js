935c157f725cd8dc07d83ce5faad832dcf13a753
baboda212 <baboda212@naver.com>
2023-02-15 15:42:15 +0900

    icon 불러오기 및 애니메이션 효과

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import 'aos/dist/aos.css';
 import logo from './logo.svg'
 import {useEffect, useState} from 'react';
-import rocket from './rocket.png'
+import rocket from './rocket.png';
+import {BsFillClockFill} from 'react-icons/bs';
+import {BsFillDropletFill} from 'react-icons/bs';
+import { FcSettings } from "react-icons/fc";
+import { FcShipped } from "react-icons/fc";
 
 function App() {
 
@@ -11,6 +15,9 @@ function App() {
   useEffect(() =>{
     AOS.init();
   })
+
+  const IconLarge = {fontSize: '80px'};
+  const IconSmall = {fontSize: '40px'};
 
   return (
     <div className="App">
@@ -50,6 +57,28 @@ function App() {
               width: '600px'
             }}
             src={rocket} alt="rocket" />
+        </div>
+      </section>
+      <section className='s5'>
+        <div>
+          <BsFillClockFill style={IconLarge}/>
+          <BsFillDropletFill style={IconSmall}/>
+          <FcSettings 
+          className='loading'
+          data-aos
+          data-aos-delay='0'
+          data-aos-duration='3000'
+          style={IconLarge}
+          />
+        </div>
+        <div>
+          <FcShipped 
+          className='drive'
+          style={IconLarge}
+          data-aos
+          data-aos-delay='1000'
+          data-aos-duration='10000'
+          />
         </div>
       </section>
     </div>
