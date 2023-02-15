574ae84d7e2f917f1c430beac8a4c86dc83d7a93
baboda212 <baboda212@naver.com>
2023-02-15 14:48:53 +0900

    react-animation-aos효과

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import AOS from 'aos';
 import './App.css';
+import 'aos/dist/aos.css';
+import logo from './logo.svg'
+import {useEffect, useState} from 'react';
 
 function App() {
+
+  //AOS초기화
+  useEffect(() =>{
+    AOS.init();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='s1'>
+        <div>
+          <h1>Animation on Scroll</h1>
+          <p>스크롤시 애니메이션 효과 주기</p>
+        </div>
+      </section>
+      <section className='s2'>
+        <div>
+          <h1 data-aos='fade-left'>section2</h1>
+        </div>
+      </section>
+      <section className='s3'>
+        <img 
+        className='logo'
+        data-aos
+        data-aos-delay='500'
+        data-aos-duration='3000'
+        
+        src={logo} alt="logo" 
+        />
+      </section>
     </div>
   );
 }
